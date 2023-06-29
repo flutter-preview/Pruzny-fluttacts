61172b328d631d1e6bd6648c4181b536c034e524
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-06-29 15:32:10 -0300

    Add default values

---
+++ lib/constants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const barColor = Color(0xff19376D);
+const primaryColor = Color(0xffA5D7E8);
+const secondaryColor = Color(0xffeeeeee);
+const backgroundColor = Color(0xff1c2130);
+const iconBackgroundColor = Color(0xff576CBC);
+const dividerColor = Color(0xff666666);
+
+const appBarTextStyle = TextStyle(
+  color: secondaryColor,
+  fontSize: 24.0,
+  fontWeight: FontWeight.bold,
+);
+
+const defaultTextStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 18.0,
+  fontWeight: FontWeight.normal,
+);
+  
+const nameStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 18.0,
+  fontWeight: FontWeight.normal,
+);
+
+const phoneStyle = TextStyle(
+  color: secondaryColor,
+  fontSize: 14.0,
+  fontWeight: FontWeight.normal,
+);
