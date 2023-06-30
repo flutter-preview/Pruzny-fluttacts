17d9b8ea399a6ba862ed5785b3583256210a52a8
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-06-30 13:40:12 -0300

    Add map conversion

--- lib/model/contact.dart
+++ lib/model/contact.dart
@@ -2,7 +2,25 @@ class Contact {
   String name;
   String email;
   String phone;
-  DateTime? birthday;
+  String? birthday;
 
   Contact({required this.name, required this.email, required this.phone, this.birthday});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic> {
+      "name": name,
+      "email": email,
+      "phone": phone,
+      "birthday": birthday,
+    };
+  }
+
+  factory Contact.fromMap(Map<dynamic, dynamic> map) {
+    return Contact(
+      name: map["name"] as String,
+      email: map["email"] as String,
+      phone: map["phone"] as String,
+      birthday: map["birthday"] as String?,
+    );
+  }
 }
